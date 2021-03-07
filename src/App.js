d328fa8c2279fde4603e3feaa00412da7c4b01a2
Christian-D-K <christian.d.k.505@gmail.com>
2021-03-07 15:51:10 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import { TimeContainer } from './components/TimeContainer';
 import { APIFetch } from './components/APIFetch'
+import { AppContext } from './contexts/AppContext'
+import { B } from './components/B';
+import { BasicReducer } from './components/BasicReducer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <APIFetch />
-      </header>
-    </div>
+    <AppContext.Provider value={'value from App.js'}>
+      <div className="App">
+        <header className="App-header">
+          {/* <APIFetch />
+          <B /> */}
+          <BasicReducer />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
 export default App;
+
